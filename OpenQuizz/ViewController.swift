acd1a892a1ccb384fc1683084bae777770c5c296
Yann Yver <yveryann@gmail.com>
2019-11-18 13:09:43 +0100

    Initial Commit

---
+++ OpenQuizz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenQuizz
+//
+//  Created by Yann Yver on 18/11/2019.
+//  Copyright © 2019 Yann Yver. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
