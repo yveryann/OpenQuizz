c30ed05214b645f833de9042026a0d56e0985446
Yann Yver <yveryann@gmail.com>
2019-11-25 12:12:20 +0100

    add controller connection

--- OpenQuizz/Controler/ViewController.swift
+++ OpenQuizz/Controler/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var newGameButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var questionView: QuestionsView!
+    
+    var game = Game()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let name = Notification.Name(rawValue: "QuestionsLoaded")
+        NotificationCenter.default.addObserver(self, selector: #selector(questionsLoaded), name: name, object: nil)
+        
+        startNewGame()
+    }
+    
+    func questionsLoaded() {
+        activityIndicator.isHidden = true
+        newGameButton.isHidden = false
+        
+        questionView.title = game.currentQuestion.title
     }
 
-
+    @IBAction func didTapNewGameButton() {
+        startNewGame()
+    }
+    
+    private func startNewGame() {
+        activityIndicator.isHidden = false
+        newGameButton.isHidden = true
+        
+        questionView.title = "Loading..."
+        questionView.style = .standard
+        
+        scoreLabel.text = "0 / 10"
+        
+        game.refresh()
+    }
+    
 }
 
